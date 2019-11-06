9c89d16562cc77f98345ebfe6243a202dd4398ae
fahimahmadi <a.fahimahmadi@gmail.com>
2019-11-06 03:19:00 +0100

    Updates

---
+++ precache-manifest.4c24c14c868a754b81f24a69fb4febe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9da1a2aa3eafd0ba7c78984fffb27a",
+    "url": "/flightapp/index.html"
+  },
+  {
+    "revision": "8e44ae0b0b4ecc21aba7",
+    "url": "/flightapp/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "d3d7a9d33c90b769b8c4",
+    "url": "/flightapp/static/js/2.677bf8ce.chunk.js"
+  },
+  {
+    "revision": "8e44ae0b0b4ecc21aba7",
+    "url": "/flightapp/static/js/main.7dbee2aa.chunk.js"
+  },
+  {
+    "revision": "4d2128ef2c6c74026c3f",
+    "url": "/flightapp/static/js/runtime-main.f4587f81.js"
+  }
+]);
